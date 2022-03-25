34e6345c96d4d8a8dcb24b22bd1179b2f1aaba01
duckhive <mbaker@duqgames.com>
2022-03-25 08:56:33 -0400

    Project prep
    Created prefabs, environment, layers, etc. to prepare for project.

---
+++ Duckey Kong/Assets/Scripts/FeedbacksManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbacksManager : MonoBehaviour
+{
+    public static FeedbacksManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
